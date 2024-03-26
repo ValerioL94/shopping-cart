4e4ed18ea84c6cfb6abf1db2072dcc7c12abd277
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-26 09:44:59 +0100

    Remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <header>
-        <h1>Shopping cart</h1>
-      </header>
-      <main></main>
-      <footer>
-        <div className="about">
-          <p>Copyright © 2024 ValerioL94</p>
-          <a href="https://github.com/ValerioL94">
-            <img
-              className="github-logo"
-              src="/assets/icons/github-mark.svg"
-              alt="github logo"
-            />
-          </a>
-        </div>
-      </footer>
-    </>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
