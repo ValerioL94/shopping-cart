8b6947957d3a07ddbcab0b7b927d7a57453ee4ef
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-29 12:41:01 +0100

    Add categories selection and update styles

--- src/routes/shop.jsx
+++ src/routes/shop.jsx
@@ -1,31 +1,58 @@
 import '../styles/shop.css';
 import { useLoaderData, Form } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Shop() {
   const { products } = useLoaderData();
-
+  const categories = [...new Set(products.map((item) => item.category))];
+  const [category, setCategory] = useState('');
   return (
     <div id="shop-page">
       <div>
         <Form id="search-form" role="search">
           <input id="search-bar" type="search" />
-          <select name="categories" id="categories">
-            <option value="">All</option>
+          <select
+            name="categories"
+            id="categories"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">all</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
           </select>
         </Form>
       </div>
       <div id="cards-container">
-        {products.map((product) => (
-          <div key={product.id} className="card" tabIndex={0}>
-            <img
-              src={product.image}
-              alt="product.description"
-              className="cardImage"
-            />
-            <h2 className="cardTitle">{product.title}</h2>
-            <p className="cardPrice">${product.price}</p>
-          </div>
-        ))}
+        {!category &&
+          products.map((product) => (
+            <div key={product.id} className="card" tabIndex={0}>
+              <img
+                src={product.image}
+                alt="product.description"
+                className="cardImage"
+              />
+              <h2 className="cardTitle">{product.title}</h2>
+              <p className="cardPrice">${product.price}</p>
+            </div>
+          ))}
+        {category &&
+          products
+            .filter((product) => product.category === category)
+            .map((product) => (
+              <div key={product.id} className="card" tabIndex={0}>
+                <img
+                  src={product.image}
+                  alt="product.description"
+                  className="cardImage"
+                />
+                <h2 className="cardTitle">{product.title}</h2>
+                <p className="cardPrice">${product.price}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
