675a710618bde3f38d55b0bdbc066d5d32c2349e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-04-05 13:52:50 +0200

    Update styles, setup vitest

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,9 +1,23 @@
 import '../styles/home.css';
 
 export default function Home() {
+  const quotes = [
+    '"We have everything you need and more"',
+    '"You need it, We got it"',
+    `"If you don't find it, just keep looking"`,
+    'Why buy tomorrow what you can buy today?',
+  ];
+
+  function randomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function getQuote(array) {
+    return array[randomInt(array.length)];
+  }
+
   return (
     <div id="home-page">
-      <h1>&quot;We have everything you need and more&quot;</h1>
+      <h1 className="quote">{getQuote(quotes)}</h1>
     </div>
   );
 }
